ff3440b95ed16ff68f1c7d81a8889fa364a0403e
Oleh Dubno <olehdubno@gmail.com>
2015-04-28 15:51:52 -0400

    moved the main scrape function to it's own .py

--- instagram_scraper/instagram_analyze.py
+++ instagram_scraper/instagram_analyze.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_template, request, flash, \
-  flash, url_for, redirect
-
-from instagram_analyze import * 
-
-"""from glasses import *
-
 from forms import InstagramScraper
-
-import pandas as pd
 import requests
 import json
-from pandas.io.json import json_normalize"""
-
-app = Flask(__name__)
-app.config.from_object('instagram_scraper.config')
+from pandas.io.json import json_normalize
+from glasses import *
 
 
-
-"""def instagram_scraper(word):
+def instagram_scraper(word):
 
     base_url = "https://api.instagram.com/v1"
     query=word
@@ -46,29 +34,3 @@ app.config.from_object('instagram_scraper.config')
           inplace=True)
     
     return df
-"""
-
-
-#routes 
-
-@app.route('/', methods=['GET','POST'])
-def main():
-  form = InstagramScraper(request.form)
-  if form.validate_on_submit():
-    text = form.instagram_scrape.data
-    return redirect(url_for('instagram_scrape', user_input=text))
-  return render_template('index.html', form=form)
-
-
-@app.route('/instagram_search/<user_input>')
-def instagram_scrape(user_input):
-  instagram_scraped = instagram_scraper(user_input)
-  return render_template(
-    'instagram_scraper.html',
-    input=user_input,
-    output=instagram_scraped
-    )
-
-@app.route('/about')
-def home():
-  return render_template('about.html')
