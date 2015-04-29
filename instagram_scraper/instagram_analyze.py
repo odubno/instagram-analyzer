80f4844e0768fc06b29b8bfe23c0d0365f4243e0
crkyo <criickyO@gmail.com>
2015-04-29 19:37:06 -0400

    fixed indent error

--- instagram_scraper/instagram_analyze.py
+++ instagram_scraper/instagram_analyze.py
@@ -14,6 +14,14 @@ def instagram_scraper(word):
     url = '{0}/tags/{1}/media/recent?client_id={2}&count=5'.format(base_url, query, client_id)
 
     df = json_normalize(requests.get(url).json()['data'])
+	
+	total_posts = len(df)
+	total_comments = df['comments.count'].sum()
+	total_likes = df['likes.count'].sum()
+
+	print total_likes, 'people like',total_posts, 'recent posts containing #'+word+'!'
+	print str(float(total_comments)/float(total_likes))[:5]+'% of Instagram users who have liked posts containing #'+word, 'have commented on a post.'
+
     
     #df = df[['user.username','caption.text','tags','comments.count','likes.count',
     #         'filter','type','created_time','user.full_name','user.id','link','location.latitude',
@@ -35,12 +43,4 @@ def instagram_scraper(word):
     #      inplace=True)
     
     #return df
-	
-	total_posts = len(df)
-	total_comments = df['comments.count'].sum()
-	total_likes = df['likes.count'].sum()
-
-	print total_likes, 'people like',total_posts, 'recent posts containing #'+word+'!'
-	print str(float(total_comments)/float(total_likes))[:5]+'% of Instagram users who have liked posts containing #'+word, 'have commented on a post.'
-
 
