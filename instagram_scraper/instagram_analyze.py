40c383342e3dc5cead19cfa26c42c38d68ae16be
crkyo <criickyO@gmail.com>
2015-04-29 19:26:30 -0400

    replaced pd.df with test stats

--- instagram_scraper/instagram_analyze.py
+++ instagram_scraper/instagram_analyze.py
@@ -15,25 +15,32 @@ def instagram_scraper(word):
 
     df = json_normalize(requests.get(url).json()['data'])
     
-    df = df[['user.username','caption.text','tags','comments.count','likes.count',
-             'filter','type','created_time','user.full_name','user.id','link','location.latitude',
-             'location.longitude']]
+    #df = df[['user.username','caption.text','tags','comments.count','likes.count',
+    #         'filter','type','created_time','user.full_name','user.id','link','location.latitude',
+    #         'location.longitude']]
     
-    df.rename(columns={'user.username':'user_name',
-                   'caption.text':'caption_text',
-                   'tags':'hashtags',
-                   'comments.count':'comments_count',
-                   'likes.count':'likes_count',
-                   'filter':'filter',
-                   'created_time':'created_time',
-                   'user.full_name':'full_name',
-                   'user.id':'user_id',
-                   'type':'type',
-                   'link':'link',
-                   'location.latitude':'latitude',
-                   'location.longitude':'longitude'},
-          inplace=True)
+    #df.rename(columns={'user.username':'user_name',
+    #               'caption.text':'caption_text',
+    #               'tags':'hashtags',
+    #               'comments.count':'comments_count',
+    #               'likes.count':'likes_count',
+    #               'filter':'filter',
+    #               'created_time':'created_time',
+    #               'user.full_name':'full_name',
+    #               'user.id':'user_id',
+    #               'type':'type',
+    #               'link':'link',
+    #               'location.latitude':'latitude',
+    #               'location.longitude':'longitude'},
+    #      inplace=True)
     
-    return df
+    #return df
+	
+	total_posts = len(df)
+	total_comments = df['comments.count'].sum()
+	total_likes = df['likes.count'].sum()
+	
+	print total_likes, 'people like',total_posts, 'recent posts containing #'+word+'!'
+	print str(int(total_comments)/int(total_likes))+'% of Instagram users who have liked posts containing #'+word, 'have commented on a post.'
 
 
