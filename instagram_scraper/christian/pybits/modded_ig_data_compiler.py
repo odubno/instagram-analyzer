0ef8a1e459445e9dc72117c505ea87386738e9a5
crkyo <criickyO@gmail.com>
2015-04-29 19:56:16 -0400

    testing loop integration

---
+++ instagram_scraper/christian/pybits/modded_ig_data_compiler.py
@@ -0,0 +1,32 @@
+#is it possible to save 'url' 
+
+from glasses import *
+
+def get_urls(url, n):
+
+      #return a next_url
+    def get(url):
+        return str(requests.get(url).json()['pagination']['next_url'])
+    
+      #open list to hold urls
+    urls = list() 
+    
+      #handling initial url
+    url = query_url #call passed url
+    urls.append(str(url)) #add initial url to list
+
+      #handling further urls    
+    for _ in range(n):
+        x = get(url) 
+        urls.append(str(x)) #add next_url
+        url = get(x) #replaces initial url with next_url for next turn in loop
+
+      #open list to hold data
+    results = list()
+
+      #populate df with data from urls in urls
+    for url in urls:
+        results.append(json_normalize(requests.get(url).json()['data']))
+
+      #initiate df
+    df = pd.DataFrame().append(results).reset_index().drop('index',axis=1)
