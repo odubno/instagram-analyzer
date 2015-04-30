4b0b58b75723bb5a1036b9f19261110a0a365548
Oleh Dubno <olehdubno@gmail.com>
2015-04-30 15:27:28 -0400

    fixed the code in instagram_analyze after forgetting to push

--- instagram_scraper/instagram_analyze.py
+++ instagram_scraper/instagram_analyze.py
@@ -1,62 +1,53 @@
 from forms import InstagramScraper
 import requests
 import json
-import pandas as pd
 from pandas.io.json import json_normalize
 from glasses import *
-<<<<<<< HEAD
 
 
-=======
-import matplotlib
-#from igDataCompiler import GetUrl
->>>>>>> eb45eefc6aeea3f5523d5ef7c53d262ef5e97520
 
 def instagram_scraper(word):
-	base_url = "https://api.instagram.com/v1"
-	query=word
-	url = '{0}/tags/{1}/media/recent?client_id={2}&count=30'.format(base_url, query, client_id)	
-	#get_pages(url, 2)
-	df = json_normalize(requests.get(url).json()['data'])
-	total_posts = len(df)
-	total_comments = df['comments.count'].sum()
-	total_likes = df['likes.count'].sum()
-	return total_likes, 'people like',total_posts, 'recent posts containing #'+word+'!'
-	return str(float(total_comments)/float(total_likes))[:5]+'% of Instagram users who have liked posts containing #'+word, 'have commented on a post.'
 
-<<<<<<< HEAD
     base_url = "https://api.instagram.com/v1"
     query=word
     errors = []
     
     try:
-        url = '{0}/tags/{1}/media/recent?client_id={2}&count=5'.format(base_url, query, client_id)
+        url = '{0}/tags/{1}/media/recent?client_id={2}&count=20'.format(base_url, query, client_id)
         df = json_normalize(requests.get(url).json()['data'])
     except:
         errors.append(
             "Error: Make sure that the search is just the word string, without spaces or hashtag signs."
             )
         return errors
-=======
-    #df = df[['user.username','caption.text','tags','comments.count','likes.count',
-    #         'filter','type','created_time','user.full_name','user.id','link','location.latitude',
-    #         'location.longitude']]
->>>>>>> eb45eefc6aeea3f5523d5ef7c53d262ef5e97520
     
-    #df.rename(columns={'user.username':'user_name',
-    #               'caption.text':'caption_text',
-    #               'tags':'hashtags',
-    #               'comments.count':'comments_count',
-    #               'likes.count':'likes_count',
-    #               'filter':'filter',
-    #               'created_time':'created_time',
-    #               'user.full_name':'full_name',
-    #               'user.id':'user_id',
-    #               'type':'type',
-    #               'link':'link',
-    #               'location.latitude':'latitude',
-    #               'location.longitude':'longitude'},
-    #      inplace=True)
+    # Cleaning up the Data Frame
+    df = df[['user.username','caption.text','tags','comments.count','likes.count',
+             'filter','type','created_time','user.full_name','user.id','link','location.latitude',
+             'location.longitude']]
     
-    #return df
+    # Changing the column names in the Data Frame
+    df.rename(columns={'user.username':'user_name',
+                   'caption.text':'caption_text',
+                   'tags':'hashtags',
+                   'comments.count':'comments_count',
+                   'likes.count':'likes_count',
+                   'filter':'filter',
+                   'created_time':'created_time',
+                   'user.full_name':'full_name',
+                   'user.id':'user_id',
+                   'type':'type',
+                   'link':'link',
+                   'location.latitude':'latitude',
+                   'location.longitude':'longitude'},
+          inplace=True)
+
+    total_posts = len(df)
+    comments_count = df['comments_count'].sum()
+    likes_count = df['likes_count'].sum()
+
+    return likes_count, "people like", total_posts, "recent posts containing #"+word+"!"
+    return str(float(comments_count)/float(likes_count)
+        )[:5]+'% of Instagram users who have liked psots containing #'+word,'have commented on a post.'
+
 
