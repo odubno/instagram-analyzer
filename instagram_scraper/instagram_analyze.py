ae030e3d70717abb4237394f8d688cf651e399c5
crkyo <criickyO@gmail.com>
2015-04-30 16:39:16 -0400

    integration

--- instagram_scraper/instagram_analyze.py
+++ instagram_scraper/instagram_analyze.py
@@ -14,22 +14,22 @@ def instagram_scraper(word):
     urls = list()
     results = list()
 	
-	def get(url):
-		return str(requests.get(url).json()['pagination']['next_url'])
+    def get(url):
+        return str(requests.get(url).json()['pagination']['next_url'])
 	  
     try:
         url = '{0}/tags/{1}/media/recent?client_id={2}&count=20'.format(base_url, query, client_id)
 		#handling initial url
-		urls.append(str(url)) #add initial url to list
+        urls.append(str(url)) #add initial url to list
 		#handling further urls    
-		for _ in range(2): #2 will later on be replaced by n, which ideally should be defined by the user the way query is
-			x = get(url) #get next_url
-			urls.append(str(x)) #add next_url to list
-			url = get(x) #replaces initial url with next_url for next turn in loop
+        for _ in range(2): #2 will later on be replaced by n, which ideally should be defined by the user the way query is
+            x = get(url) #get next_url
+            urls.append(str(x)) #add next_url to list
+            url = get(x) #replaces initial url with next_url for next turn in loop
 		#populating df
-		for url in urls:
-			results.append(json_normalize(requests.get(url).json()['data']))
-		df = pd.DataFrame().append(results).reset_index().drop('index',axis=1)
+        for url in urls:
+            results.append(json_normalize(requests.get(url).json()['data']))
+        df = pd.DataFrame().append(results).reset_index().drop('index',axis=1)
     except:
         errors.append(
             "Error: Make sure that the search is just the word string, without spaces or hashtag signs."
