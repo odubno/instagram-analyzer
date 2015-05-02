bf35875f8e68516240c61e2d5315191491badbd6
crkyo <criickyO@gmail.com>
2015-05-02 04:04:10 -0400

    adding foundation for twitter stream functionality

---
+++ instagram_scraper/christian/extend/fightnight/creds.py
@@ -0,0 +1,5 @@
+ckey = '4IZDtQJKv0YdwMNdXETZnSq9Y'
+csecret = 'ccD3DHRsXLSWXMr1V9vZ3dspF8KPvfWLTFB4Kvk21zk3inMeTi'
+atoken = '262034478-mwhM0KKB0h6M45IxWyCmpCAvwj1rkxjiRl6ArANy'
+asecret = 'y75xQdvPoWIibYmM9i4ZkjEUk5x7xCPdqHg0mErbUMCQJ'
+
