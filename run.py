ea6435b732d4340c87cdc04aaae4c46a6dcba1ee
Oleh Dubno <olehdubno@gmail.com>
2015-05-12 12:28:26 -0400

    deploying the app

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 from instagram_scraper import app
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
+  # port = int(os.environ.get('PORT', 5000))
   app.run(debug=True)
