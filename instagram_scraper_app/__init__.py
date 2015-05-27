6c4cbda54b7a5f6f5d460dcda53b5be6d395e688
Oleh Dubno <olehdubno@gmail.com>
2015-05-27 15:09:43 -0400

    fixed the bug

--- instagram_scraper_app/__init__.py
+++ instagram_scraper_app/__init__.py
@@ -25,14 +25,14 @@ def main():
   return render_template('index.html', form=form)
 
 
-# @app.route("/instagram_scrape/<user_input>") # 1
-# def instagram_scrape(user_input):
+@app.route("/instagram_scrape/<user_input>") # 1
+def instagram_scrape(user_input):
 
-#   return render_template(
-#     'instagram_scraper.html',
-#     input=user_input,
-#     filename=user_input+".png" # 2
-#     )
+  return render_template(
+    'instagram_scraper.html',
+    input=user_input,
+    filename=user_input+".png" # 2
+    )
 
 """
 The beginning of the route @app.route("/instagram_scrape/<user_input>") picks 
@@ -47,47 +47,47 @@ the user_input with the ".png" ending
 
 """
 
-# @app.route("/instagram_scrape/<image_name>.png") # 3
-# def image(image_name):
-#   # pulls in the scraper and creates the DataFrame
-#   instagram_scraped = instagram_scraper(image_name, 0)
-
-#   # formats the DataFrame to display plots 
-#   instagram_graph(instagram_scraped)
-
-
-#   # rendering matplotlib image to Flask view
-#   canvas = FigureCanvas(plt.gcf())
-#   output = StringIO()
-#   canvas.print_png(output)
-#   # make_response converts the return value from a view 
-#   # function to a real response object that is an instance 
-#   # of response_class.
-#   response = make_response(output.getvalue())
-
-#   response.mimetype = 'image/png'
-
-
-#   return response 
-
-@app.route("/instagram_scrape/<user_input>") # 3
-def image(user_input):
+@app.route("/instagram_scrape/<image_name>.png") # 3
+def image(image_name):
   # pulls in the scraper and creates the DataFrame
-  df = instagram_scraper(user_input, 0)
+  instagram_scraped = instagram_scraper(image_name, 0)
 
-  df = df.dropna()
-  df = df[['Location Latitude', 'Location Longitude']]
-  df = np.array(df.as_matrix(columns = None)).astype(np.float)
+  # formats the DataFrame to display plots 
+  instagram_graph(instagram_scraped)
 
-  map = gmaps.heatmap(df_array)
 
-  map_x = gmaps.display(map)
+  # rendering matplotlib image to Flask view
+  canvas = FigureCanvas(plt.gcf())
+  output = StringIO()
+  canvas.print_png(output)
+  # make_response converts the return value from a view 
+  # function to a real response object that is an instance 
+  # of response_class.
+  response = make_response(output.getvalue())
 
-  return render_template(
-    'instagram_scraper.html',
-    input=user_input,
-    filename=map_x # 2
-    )
+  response.mimetype = 'image/png'
+
+
+  return response 
+
+# @app.route("/instagram_scraper/<user_input>") # 3
+# def image(user_input):
+#   # pulls in the scraper and creates the DataFrame
+#   df = instagram_scraper(user_input, 0)
+
+#   df = df.dropna()
+#   df = df[['Location Latitude', 'Location Longitude']]
+#   df = np.array(df.as_matrix(columns = None)).astype(np.float)
+
+#   map = gmaps.heatmap(df_array)
+
+#   map_x = gmaps.display(map)
+
+#   return render_template(
+#     'instagram_scraper.html',
+#     input=user_input,
+#     filename=map_x # 2
+#     )
 
 
 
